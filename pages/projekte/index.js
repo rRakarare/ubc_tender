8e2666369cd104336a42cac0def2b10c24a44487
Rene Couturier <rRene333@googlemail.com>
2022-03-21 15:32:42 +0100

    intro projekte

--- pages/projekte/index.js
+++ pages/projekte/index.js
@@ -1,72 +1,81 @@
-import { Badge, Box, HStack, Icon, Stack, Tag, Text } from "@chakra-ui/react";
-import { Select, chakraComponents } from "chakra-react-select";
+import {
+  Box,
+  chakra,
+  Flex,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  StatNumber,
+  useColorModeValue,
+  Icon,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { BsPerson } from "react-icons/bs";
+import { FiServer } from "react-icons/fi";
+import { GoLocation } from "react-icons/go";
 
-import prisma from "../../lib/prisma";
-
-const customComponents = {
-  Option: ({ children, ...props }) => {
-
-      return (
-        <chakraComponents.Option {...props}>
-          <HStack spacing={4}>
-            <Text>{children}</Text>
-            {props.data.tags.map((tag) => (
-              <Tag size={"sm"} key={tag.name} variant="solid" colorScheme={tag.colorSchema}>
-                {tag.name}
-              </Tag>
-            ))}
-          </HStack>
-        </chakraComponents.Option>
-      )
-  }
-};
-
-export async function getStaticProps() {
-  const query = await prisma.project.findMany({
-    include: {
-      projectType: true,
-      TagsOnProjects: {
-        include: {
-          tag: true,
-        },
-      },
-    },
-  });
-
-  const data = query.map((item) => {
-    return {
-      id: item.id,
-      name: item.name,
-      projectType: item.projectType,
-      tags: item.TagsOnProjects.map((entry) => entry.tag),
-    };
-  });
-
-  return {
-    props: {
-      data: data,
-    },
-  };
+function StatsCard(props) {
+  const { title, stat, icon, link } = props;
+  return (
+    <Link href={link}>
+      <Stat
+        px={{ base: 2, md: 4 }}
+        py={"5"}
+        shadow={"xl"}
+        border={"1px solid"}
+        borderColor={props.bColor}
+        borderWidth={3}
+        rounded={"lg"}
+        role="group"
+        _hover={{
+          backgroundColor: props.bColor,
+          color: "white",
+          cursor: "pointer",
+        }}
+      >
+        <Flex justifyContent={"space-between"}>
+          <Box pl={{ base: 2, md: 4 }}>
+            <StatLabel fontWeight={"medium"} isTruncated>
+              {title}
+            </StatLabel>
+            <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
+              {stat}
+            </StatNumber>
+          </Box>
+          <Box
+            my={"auto"}
+            color={useColorModeValue("gray.800", "gray.200")}
+            alignContent={"center"}
+          >
+            {icon}
+          </Box>
+        </Flex>
+      </Stat>
+    </Link>
+  );
 }
 
-export default function Projekte({ data }) {
-
-
-
-  const dataOptions = data.map(item => ({
-      value: item.id,
-      label: item.name,
-      tags: item.tags
-  }))
-
-  console.log(dataOptions);
-
+export default function Projekte() {
   return (
-    <Select
-      name="flavors"
-      options={dataOptions}
-      placeholder="Select some flavors..."
-      components={customComponents}
-    />
+    <Box maxW="7xl" mx={"auto"} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 5, lg: 8 }}>
+        <StatsCard
+          link={"/"}
+          bColor={"#655B8E"}
+          title={"Edit Project"}
+          icon={
+            <Icon as={BsPerson} _groupHover={{ color: "white" }} w={6} h={6} />
+          }
+        />
+        <StatsCard
+          link={"/projekte/load"}
+          bColor={"#4B7D81"}
+          title={"New Project"}
+          icon={
+            <Icon as={BsPerson} _groupHover={{ color: "white" }} w={6} h={6} />
+          }
+        />
+      </SimpleGrid>
+    </Box>
   );
 }
