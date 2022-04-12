9081448516b1b32446d0d5c7bc58f09ef6e57877
Rene Couturier <rRene333@googlemail.com>
2022-04-12 15:11:16 +0200

    saddsasadweq

--- components/Project/Navigation.js
+++ components/Project/Navigation.js
@@ -8,15 +8,16 @@ import {
   VStack,
   HStack,
 } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, Icon } from "@chakra-ui/icons";
 import Link from "next/link";
+import { FaRegSave } from "react-icons/fa";
 
 const navs = [
   { name: "Main", href: "/" },
   { name: "Contents", href: "/content" },
 ];
 
-export default function Navigation({ path }) {
+export default function Navigation({ path, submitter }) {
   const Navs = navs.map((item) => {
     return (
       <Link key={item.href} href={`/projekte/${path.id}${item.href}`}>
@@ -26,14 +27,17 @@ export default function Navigation({ path }) {
   });
 
   return (
-    <Menu>
-      <HStack spacing={4}>
-        <MenuButton bg={"white"} as={Button}>
-          <HamburgerIcon />
-        </MenuButton>
-        <Text>{path.name}</Text>
-      </HStack>
-      <MenuList>{Navs}</MenuList>
-    </Menu>
+    <HStack justify={"space-between"}>
+      <Menu>
+        <HStack spacing={4}>
+          <MenuButton bg={"white"} as={Button}>
+            <HamburgerIcon />
+          </MenuButton>
+          <Text>{path.name}</Text>
+        </HStack>
+        <MenuList>{Navs}</MenuList>
+      </Menu>
+      <Icon onClick={()=>submitter()} cursor={"pointer"} w={8} h={8}  as={FaRegSave}/>
+    </HStack>
   );
 }
--- pages/projekte/[id]/index.js
+++ pages/projekte/[id]/index.js
@@ -1,11 +1,21 @@
 import Navigation from "../../../components/Project/Navigation";
 import prisma from "../../../lib/prisma";
+import { useForm, useController } from "react-hook-form";
+import {
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  useBoolean,
+} from "@chakra-ui/react";
 
 export async function getServerSideProps({ params }) {
   const projectId = Number(params.id);
 
   const project = await prisma.project.findFirst({
     where: { id: projectId },
+    include: { client: true },
   });
 
   return {
@@ -14,11 +24,77 @@ export async function getServerSideProps({ params }) {
 }
 
 export default function Project({ project }) {
+  console.log(project);
+
+  const defaultValues = project;
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ defaultValues });
+  const [isLoading, setLoading] = useBoolean(false);
+
+  const submit = async (data) => {
+    setLoading.on();
+    setTimeout(() => {
+      setLoading.off();
+      alert(JSON.stringify(data, null, 2));
+      console.log(data);
+    }, 1);
+  };
 
   return (
     <>
-      <Navigation path={{name:"Main", id:project.id}} />
-      <div>{project.name}</div>
+      <Navigation
+        path={{ name: "Main", id: project.id }}
+        submitter={handleSubmit(submit)}
+      />
+      <Container
+        mt={6}
+        as="form"
+        bg={"white"}
+        p={6}
+        onSubmit={handleSubmit(submit)}
+      >
+        <FormControl pb={4} isInvalid={errors.name}>
+          <FormLabel htmlFor="name">Projectname</FormLabel>
+          <Input id="name" placeholder="name" {...register("name", {})} />
+          <FormErrorMessage>
+            {errors.name && errors.name.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl pb={4} isInvalid={errors.name}>
+          <FormLabel htmlFor="name">Description</FormLabel>
+          <Input id="service" placeholder="service" {...register("service", {})} />
+          <FormErrorMessage>
+            {errors.service && errors.service.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl pb={4} isInvalid={errors.name}>
+          <FormLabel htmlFor="name">Ref. Nr.</FormLabel>
+          <Input id="ref" placeholder="ref" {...register("ref", {})} />
+          <FormErrorMessage>
+            {errors.ref && errors.ref.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl pb={4} isInvalid={errors.name}>
+          <FormLabel htmlFor="name">Shorthand</FormLabel>
+          <Input id="short" placeholder="short" {...register("short", {})} />
+          <FormErrorMessage>
+            {errors.short && errors.short.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl pb={4} isInvalid={errors.name}>
+          <FormLabel htmlFor="name">Year</FormLabel>
+          <Input id="year" placeholder="year" {...register("year", {})} />
+          <FormErrorMessage>
+            {errors.year && errors.year.message}
+          </FormErrorMessage>
+        </FormControl>
+      </Container>
     </>
   );
 }
